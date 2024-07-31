d8564d6c3b8001b92c537c437a518800b8f86bd6
inderdeep <deep3576@gmail.com>
2024-07-31 00:31:18 -0400

    Update

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Exit script on any error
+set -e
+
+# Function to print messages
+print_message() {
+    echo -e "\n\033[1;32m$1\033[0m"
+}
+
+
+print_message "Upgrading pip..."
+# Upgrade pip in the virtual environment
+pip3 install --upgrade pip
+
+print_message "Installing dependencies from requirements.txt..."
+# Install the required packages
+pip3 install -r requirements.txt
+
+print_message "Running the Flask application..."
+# Run the Flask application
+print_message "application will be running on port 8000"
+print_message "http://localhost:8000"
+
+gunicorn -w 4 -b 0.0.0.0:8000 app:app
